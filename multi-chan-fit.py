62ce6ce4ec08559fe3d0c2331f864a4cb3287149
mkauer <mkauer@icecube.wisc.edu>
2016-11-22 16:50:44 -0600

    updated to 02-mega-plot-test.py

--- multi-chan-fit.py
+++ multi-chan-fit.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python
 
+############################################################
+#
+# version: 2016-09-14
+#
+# Change Log
+#-----------------------------------------------------------
+# + functionized some things in prep for the future fitting
+# + ROOT batch settings [0 or 1]
+# + canvas as a python list
+# + histos as a python list
+# + hacking at multi-plot-test.py
+# + hacking at plot-test.py
+#
+# mkauer@physics.wisc.edu
+############################################################
+
 import os,sys
 import numpy as np
 from ROOT import *
 import ROOT
+
+
+noShow = 0  ### 0 == show-plots  1 == dont-show-plots
+gROOT.SetBatch(noShow)
 
 def _myself_(argv):
     
@@ -14,54 +34,120 @@ def _myself_(argv):
     
     gStyle.SetPadBottomMargin(0.12)
     gStyle.SetPadLeftMargin  (0.11)
-    #gStyle.SetPadTopMargin   (0.02)
     gStyle.SetPadRightMargin (0.05)
-    
     gStyle.SetTitleOffset(1.2,"x")
     gStyle.SetTitleOffset(1.0,"y")
     gStyle.SetTitleSize(0.045,"xy")
     gStyle.SetLabelSize(0.040,"xy")
     
     ch1=TChain("MC","")
-
-    ch1.Add("sim/data/K40/set2_internalK40-1-nEv200000.root")
+    #ch1.Add("sim/data/K40/set2_internalK40-1-nEv200000.root")
+    ch1.Add("sim/data/K40/set2_internalK40-2-nEv200000.root")
+    #ch1.Add("sim/data/K40/*.root")
     
+    volnames = ['NaIDet01Crystal', 'NaIDet02Crystal', 'NaIDet03Crystal', 'NaIDet04Crystal',
+                'NaIDet05Crystal', 'NaIDet06Crystal', 'NaIDet07Crystal', 'NaIDet08Crystal']
+    
+    par = histparam()
+    canvs = []
+    data = getData(["data/dryRun/*.root"])
+    
+    #for i in range(0,8): # is primary crystal of origin
+    for i in range(0,1): # is primary crystal of origin
+        
+        sims = []
+        for j in range(0,8): # is surounding crystals and self
+            sim = TH1F("sim", names(j), par[0], par[1], par[2])
+            cut1 = TCut('event_MC.edepResol['+str(j)+']*1000. > 0')
+            cut2 = TCut('event_MC.primVolumeName == "'+volnames[i]+'"')
+            ch1.Draw('event_MC.edepResol['+str(j)+']*1000. >> sim',cut1+cut2)
+            sim.Sumw2()
+            sims.append(sim)
+        
+        canv = TCanvas(names(i), names(i), 0, 0, 800, 800)
+        canvs.append(canv)
+        canvs[i].Divide(3,3)
+        for k,sim in enumerate(sims):
+            canvs[i].cd(k+1)
+            canvs[i].cd(k+1).SetLogy()
+            if k == i:
+                sim.SetMarkerColor(kRed+1)
+                sim.SetLineColor(kRed+1)
+            else:
+                sim.SetMarkerColor(kBlue+1)
+                sim.SetLineColor(kBlue+1)
+            sim.SetLineWidth(1)
+            #sim.Draw("same")
+            #sim.Draw()
+            #dat.Draw("same")
+            data[k].Draw()
+            sim.Draw("same")
+            
+        canvs[i].Update()
+        canvs[i].Print('testing-'+names(i)+'.png')
+                
+    # pause here?
+    if not noShow:
+        raw_input("[Enter] to continue...\n")
+
+
+
+def getData(rootfiles=['data/dryRun/*.root']):
+    chain = TChain("ntp","")
+    for rfile in rootfiles:
+        chain.Add(rfile)
+    data = []
+    par = histparam()
+    for i in range(0,8):
+        dat = TH1F("dat", names(i), par[0], par[1], par[2])
+        chain.Draw('crystal'+str(i+1)+'.qc *'+str(calib(i))+' >> dat')
+        dat.Sumw2()
+        dat.SetLineColor(kBlack)
+        dat.SetMarkerColor(kBlack)
+        dat.SetLineWidth(1)
+        data.append(dat)
+    return data
+
+
+def calib(i):
+    """
+    Return the calibrations for the crystals
+    """
+    calibrations = [0.000324,
+                    0.000324,
+                    0.000324,
+                    0.000324,
+                    0.000324,
+                    0.000324,
+                    0.000324,
+                    0.000324]
+    return calibrations[int(i)]
+
+
+def names(i):
+    """
+    The names of the crystals
+    """
+    crystals = ['NaI-01',
+                'NaI-02',
+                'NaI-03',
+                'NaI-04',
+                'NaI-05',
+                'NaI-06',
+                'NaI-07',
+                'NaI-08']
+    return crystals[int(i)]
+
+
+def histparam():
+    """
+    Histogram parameters for number of bins, min, and max in keV
+    """
     hmin = 0
     hmax = 1800
-    R = 2
-    bins = (hmax-hmin)/R
+    bins = (hmax-hmin)/2
+    return [bins, hmin, hmax]
 
-    volnames = ['NaIDet01Crystal','NaIDet02Crystal','NaIDet03Crystal','NaIDet04Crystal',
-                'NaIDet05Crystal','NaIDet06Crystal','NaIDet07Crystal','NaIDet08Crystal']
-    names = ['NaI-01','NaI-02','NaI-03','NaI-04',
-             'NaI-05','NaI-06','NaI-07','NaI-08']
-    sims=[]
-    for i in range(0,8):
-        sim = TH1F("sim", names[i], bins, hmin, hmax)
-        ch1.Draw('edepResol['+str(i)+']*1000. >> sim','edepResol['+str(i)+']*1000. > 0 && primVolumeName == "'+volnames[0]+'"')
-        sims.append(sim)
-    
-    c1 = TCanvas("c1","c1",0,0,800,800)
-    c1.Divide(3,3)
-    for i,sim in enumerate(sims):
-        i=i+1
-        c1.cd(i)
-        c1.cd(i).SetLogy()
-        
-        sim.SetMarkerColor(kRed+1)
-        sim.SetLineColor(kRed+1)
-        sim.SetLineWidth(1)
-        #sim.Draw("same")
-        sim.Draw()
-        
-    c1.Update()
-    #c1.Print("pytest-fitting.png")
-    
 
-    
-    # see plots hack
-    raw_input("[Enter] to quit\n")
-
-    
 if __name__ == "__main__":
     _myself_(sys.argv[1:])
